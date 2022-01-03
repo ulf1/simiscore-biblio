7710b9dc10a601dba55dc31fe9bf7196cc08b3c4
luise koehler <kunstloses_brot@gmx.de>
2022-01-03 09:44:36 +0100

    implement sledgehammer scoring

--- app/metadata_scorer.py
+++ app/metadata_scorer.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Dict
+from typing import Dict, List, Set
 
 import datasketch
 import kshingle
@@ -12,6 +12,27 @@ class MetaDataSimilarityScorer:
     def compute_similarity_matrix(
         self, query_sents: Dict[uuid.UUID, str]
     ) -> Dict[str, list]:
-        ids = []
-        similarity_matrix = []
+        ids = list(query_sents.keys())
+        minhash_table = self._fill_minhash_table(query_sents.values())
+        similarity_matrix = [
+            [
+                minhash_table[i].jaccard(minhash_table[j])
+                for j in range(len(minhash_table))
+            ]
+            for i in range(len(minhash_table))
+        ]
         return {"ids": ids, "matrix": similarity_matrix}
+
+    def _fill_minhash_table(self, query_sents: str) -> List[datasketch.MinHash]:
+        minhash_table = []
+        for sentence in query_sents:
+            shingle_set = kshingle.shingleset_k(sentence, self.max_k)
+            minhash = self._hash_shingle_set(shingle_set)
+            minhash_table.append(minhash)
+        return minhash_table
+
+    def _hash_shingle_set(self, shingle_set: Set[str]) -> datasketch.MinHash:
+        minhash = datasketch.MinHash(num_perm=256)
+        for shingle in shingle_set:
+            minhash.update(shingle.encode("utf-8"))
+        return minhash
